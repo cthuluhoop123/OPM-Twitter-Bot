ad4a5ecf4da09b03fb95b44aa884d1440b0b8a18
cthuluhoop123 <dowzhong@gmail.com>
2016-12-30 15:23:45 +1100

    First commit

--- bot.js
+++ bot.js
@@ -7,18 +7,18 @@ const colors = require('colors')
 const translate = require('google-translate-api');
 
 const T = new Twit({
-  consumer_key:         'bjFtRdNc7wasg4CVMlLlfC26u',
-  consumer_secret:      'WZSeUXthMEX8C5wxMsIapilfVXCAvo1IedkecxwzaSda8S9Mdw',
-  access_token:         '812957194735915008-dYOtbwT7YBbSW35VuHlbPZTIDbB54S9',
-  access_token_secret:  'Uk4Q7HAb5Wh17OOBg6Y89DZ7Lydq7plrCh6ig5qgh9eoB',
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  '',
 })
 
 const r = new snoowrap({
-  userAgent: 'KishichiScript',
-  clientId: 'yztQBv5Pegt8DQ',
-  clientSecret: 'MqL2xkBDAh74WCA2Plc8cFjI0rc',
+  userAgent: '',
+  clientId: '',
+  clientSecret: '',
   username: 'kishichi',
-  password: 'smokeweed3'
+  password: ''
 });
 
 var stream = T.stream('statuses/filter', { follow: '173049825' })
